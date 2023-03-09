14f6e140ccea404a422b0d38b421c76c36b04b0f
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-03-08 17:29:51 -0800

    added tools and sidebar components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 // components
 import FilterBar from "@/components/filter-bar/filter-bar.component";
 import Recipes from "@/components/recipes/recipes.component";
+import Sidebar from "@/components/side-bar/side-bar.component";
+import Tools from "@/components/tools/tools.component";
 
 export const Home = () => {
   return (
     <>
       <FilterBar />
+      <Sidebar />
       <Recipes />
+      <Tools />
     </>
   )
 }
