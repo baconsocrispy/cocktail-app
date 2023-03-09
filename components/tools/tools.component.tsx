bb24ff4f3f00c8b76333a089bc13ec38849b2730
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-03-09 11:12:27 -0800

    added options to toolbar

--- components/tools/tools.component.tsx
+++ components/tools/tools.component.tsx
@@ -2,7 +2,20 @@
 
 const Tools = () => {
   return (
-    <div className="tools">Tools</div>
+    <div className="tools">
+      <div className="tools__option">
+        Cabinets
+      </div>
+      <div className="tools__option">
+        Favorites
+      </div>
+      <div className="tools__option">
+        Recipes
+      </div>
+      <div className="tools__option">
+        New Recipe
+      </div>
+    </div>
   )
 }
 
