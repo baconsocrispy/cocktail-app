cc33fb6d36b24afd7d2563879cc3bc641d7dc9b1
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-03-09 12:06:56 -0800

    created card component

--- components/recipes/recipes.component.tsx
+++ components/recipes/recipes.component.tsx
@@ -1,8 +1,16 @@
+import Card from "../card/card.component"
 
 
 const Recipes = () => {
+  var cards = [];
+  for (let i = 0; i < 60; i++) {
+    cards.push(<Card />) 
+  }
+  
   return (
-    <div className="recipes">Recipes</div>
+    <div className="recipes">
+      { cards }
+    </div>
   )
 }
 
