b9a1ad381d45965100edbba80d0f0fc3c03401e2
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-03-10 18:18:15 -0800

    created ingredient context

--- contexts/ingredients.context.tsx
+++ contexts/ingredients.context.tsx
@@ -1,3 +1,8 @@
+// external imports
+import { fetchAllIngredients } from '@/pages/api/cocktail-api';
+import { createContext, useEffect, useState, ReactNode } from 'react';
+
+// types
 export type Ingredient = {
   abv?: string;
   age?: number;
@@ -8,4 +13,42 @@ export type Ingredient = {
   product?: string;
   sub_type: string;
   updated_at: string;
+}
+
+type IngredientsContextProps = {
+  ingredients: Ingredient[]
+}
+
+type IngredientsProviderProps = {
+  children: ReactNode;
+}
+
+// context
+export const IngredientsContext = createContext<IngredientsContextProps>({
+  ingredients: []
+})
+
+// provider
+export const IngredientsProvider = ({ children }: IngredientsProviderProps) => {
+  // state
+  const [ ingredients, setIngredients ] = useState<Ingredient[]>([]);
+
+  // set ingredients
+  useEffect(() => {
+    const getIngredients = async () => {
+      const response = await fetchAllIngredients();
+      const { ingredients } = response;
+      setIngredients(ingredients)
+    }
+    getIngredients();
+  })
+
+  // export data
+  const value = { ingredients }
+
+  return (
+    <IngredientsContext.Provider value={ value }>
+      { children }
+    </IngredientsContext.Provider>
+  )
 }
--- pages/api/cocktail-api.ts
+++ pages/api/cocktail-api.ts
@@ -1,5 +1,6 @@
 // types
 import { Category } from "@/contexts/categories.context";
+import { Ingredient } from "@/contexts/ingredients.context";
 import { Recipe } from "@/contexts/recipes.context";
 
 export type RecipesAPI = {
@@ -9,6 +10,10 @@ export type RecipesAPI = {
 
 export type CategoriesAPI = {
   categories: Category[]
+}
+
+export type IngredientsAPI = {
+  ingredients: Ingredient[]
 }
 
 // api calls
@@ -22,5 +27,11 @@ export const fetchAllCategories = async () => {
   const response = await fetch('http://localhost:3001/categories');
   const categories: CategoriesAPI = await response.json();
   return categories
+}
+
+export const fetchAllIngredients = async () => {
+  const response = await fetch('http://localhost:3001/ingredients');
+  const ingredients: IngredientsAPI = await response.json();
+  return ingredients
 }
 
